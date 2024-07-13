6809e81c223c56f3ce95c45980cac98482629420
Diogo Sousa <diogosousainf@gmail.com>
2024-07-13 17:46:38 +0100

    s

--- src/app/project.service.ts
+++ src/app/project.service.ts
@@ -8,7 +8,7 @@ import { Project } from './project';
   providedIn: 'root'
 })
 export class ProjectService {
-  private apiURL = 'http://13.38.227.173/api/projects';
+  private apiURL = 'http://3.252.238.132/api/projects';
   private httpOptions = {
     headers: new HttpHeaders({
       'Content-Type': 'application/json'
